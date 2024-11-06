c9481445275d2b6630d835315238774f38395a2d
Mohan <umohanbabu107@gmail.com>
2024-11-06 23:03:57 +0530

    Add today's course summary

--- src/trainer/Day1.java
+++ src/trainer/Day1.java
@@ -4,5 +4,10 @@ public class Day1 {
     public static void main(String args[])
     {
         System.out.println("Hello java");
+        gitSetUp();
+    }
+
+    public static void gitSetUp(){
+        System.out.println("We have done git setup in this class");
     }
 }
